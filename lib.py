6a8a6d6ab29221d87b5509debf3c10b2db5b65fd
ann <bakalova91@bk.ru>
2022-10-20 22:01:00 +0500

    Тестируем

--- lib.py
+++ lib.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import test
 
 
@@ -32,3 +33,35 @@ def repeat(lists):
 
 
 repeat(test.my_input())
+=======
+def repeat(l):
+        lists = l
+        i = 0
+        index = 0
+        min = len(lists)
+        for list in lists:
+            if len(list) > min:
+                min = len(list)
+                index = i
+            i += 1
+        rep = []
+        run = True
+        j = 0
+        i = 0
+        while run:
+            try:
+                for i in range(len(lists)):
+                    for j in range(len(lists[i])):
+                        if lists[i][j] in lists[i + 1]:
+                            if lists[i] in rep:
+                                None
+                            else:
+                                rep.append(lists[i][j])  # массив с повтор. элементами
+            except IndexError:
+                run = False
+        if len(rep) > 0:
+            print('Количество одинаковых элементов: ', len(rep))
+            print('Oдинаковыe элементы: ', rep)
+        else:
+            print('Одинаковых элементов нет')
+>>>>>>> Stashed changes
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 def my_input():
     l = list()
     n = int(input("Количество входных списков: "))
@@ -5,3 +6,13 @@ def my_input():
         l.append(input('Элементы списка через пробел: ').split(' '))
         n -=1
     return l
+=======
+from lib import repeat
+
+l = list()
+n = int(input('Количество входных списков: '))
+while n > 0:
+    l.append(input('Элементы списка через пробел: ').split(' '))
+    n -=1
+repeat(l)
+>>>>>>> Stashed changes
